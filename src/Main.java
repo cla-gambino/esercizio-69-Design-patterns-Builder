9805a74fdca60e311932ee7e7ae11f53fbbacb30
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-19 17:29:47 +0100

    update person

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Person mySelf = new Person.Builder("Claudio", "Gambino")
+                .age(38)
+                .address("Via Mario Rossi, 1")
+                .build();
+
+        Person person = new Person.Builder("Mario", "Balotelli")
+                .age(25)
+                .build();
+
+
+        System.out.println("myself:");
+        System.out.println("firstName: " + mySelf.getFirstName());
+        System.out.println("secondName: " + mySelf.getLastName());
+        System.out.println("age: " + mySelf.getAge());
+        System.out.println("address: " + mySelf.getAddress());
+
+        System.out.println("person:");
+        System.out.println("firstName: " + person.getFirstName());
+        System.out.println("secondName: " + person.getLastName());
+        System.out.println("age: " + person.getAge());
+        System.out.println("address: " + person.getAddress());
+    }
+
+}
+
+class Person {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String address;
+
+
+    Person(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.address = builder.address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public static class Builder {
+
+        String firstName;
+        String lastName;
+        Integer age = 0;
+        String address = "";
+
+        public Builder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Builder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
         }
     }
 }
